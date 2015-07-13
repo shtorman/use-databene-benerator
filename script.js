1f4f9fc3266731896ac7b36c743250e81300b903
shtorman <shtorman@mail.ru>
2015-07-13 03:43:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toLink (str) {
+	var space = '';
+	str = str.toLowerCase();
+	var transl = {
+	'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 
+	'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+	'о': 'o', 'п': 'p', 'р': 'r','с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
+	'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sh','ъ': space, 'ы': 'y', 'ь': space, 'э': 'e', 'ю': 'yu', 'я': 'ya'
+	}
+	var link = '';
+	for (var i = 0; i < str.length; i++) {
+	if(/[а-яё]/.test(str.charAt(i))) { //если текущий символ - русская буква, то меняем его
+	link += transl[str.charAt(i)];
+	} else if (/[a-z0-9]/.test(str.charAt(i))) {
+	link += str.charAt(i); //если текущий символ - английская буква или цифра, то оставляем как есть
+	} else {
+	if (link.slice(-1) !== space) link += space; // если не то и не другое то вставляем space
+	}
+	}
+	return link;
+}
+
+function cut(str, cutStart, cutEnd){
+	  return str.substr(cutStart,cutEnd);
+	}
---
+++ truncate_tables.mysql.sql
@@ -0,0 +1,10 @@
+SET foreign_key_checks = 0;
+
+--truncate table s_person;
+--truncate table s_job_title;
+--truncate table s_organization;
+--truncate table s_department;
+
+--truncate table t_employee;
+
+SET foreign_key_checks = 1;
